4bd2ba972d0fc1f3fdea1f64b99cf47b193b5239
vale <valentsi@metropolia.fi>
2025-09-06 13:27:57 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ TehtavatMod1/funktio/6.1.tehtava.py
@@ -0,0 +1,23 @@
+#Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen <
+# nopan silmäluvun väliltä 1..6.
+# Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen. Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+import random
+
+def nopanheitto():
+    silmäluku = random.randint(1,6)
+    print(f"tulos on : {silmäluku}")
+    return silmäluku
+
+#nopanheitto()
+
+app_running = True
+while app_running:
+    silmäluku = nopanheitto()
+    nopanheitto()
+    if silmäluku == 6:
+        app_running = False
+
+
+
+
